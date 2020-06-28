0ac6ddec99dfacb4be95e92dafa3d8212b78f33f
Jerred Shepherd <shepherdjerred@gmail.com>
2020-06-27 20:45:51 -0700

    Fix formatting

--- src/main/java/com/shepherdjerred/minecraft/stmessages/Main.java
+++ src/main/java/com/shepherdjerred/minecraft/stmessages/Main.java
@@ -3,6 +3,4 @@ package com.shepherdjerred.minecraft.stmessages;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
-public class Main extends JavaPlugin {
-
-}
+public class Main extends JavaPlugin {}
